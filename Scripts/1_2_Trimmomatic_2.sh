6fbc1d103956ea28b074deba66838996be651e87
Petter Byström <92719116+Petbys@users.noreply.github.com>
2023-04-11 11:28:37 +0200

    Update 1_2_Trimmomatic_2.sh

--- Scripts/1_2_Trimmomatic_2.sh
+++ Scripts/1_2_Trimmomatic_2.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 15:00 --qos=short
-#SBATCH -J FastQC_Nanopore_Petter
+#SBATCH -J Trimmomatic_Petter
 #SBATCH --mail-type=All
 #SBATCH --mail-user petter.bystrom.8041@student.uu.se
 
@@ -13,10 +13,9 @@
 module load bioinfo-tools
 
 module load trimmomatic
-
 # Your commands
 
 #Settings for trimmming paired end reads
 
-trimmomatic PE -phred33 /home/pette/raw_data/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz /home/pette/raw_data/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_forward_paired.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_forward_unpaired.fq.gz //home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_reverse_paired.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+trimmomatic PE -phred33 /home/pette/raw_data/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz /home/pette/raw_data/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_forward_paired.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_forward_unpaired.fq.gz //home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_reverse_paired.fq.gz /home/pette/Genome_Analysis_Local/Analyses/01_preprocessing/Trimmomatic/Illumina_trimmed/E745-1.L500_SZAXPI015146-56_clean_trim_reverse_unpaired.fq.gz ILLUMINACLIP:/sw/bioinfo/trimmomatic/0.39/rackham/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
 
